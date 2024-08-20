2199cc9b028c88f56f771131740cfa5755b22b01
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-08-20 18:53:03 +0800

    Add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # Add any custom fields here
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+    social_media = models.URLField(blank=True, null=True)  
+    
+    
+    # Other custom fields as needed
