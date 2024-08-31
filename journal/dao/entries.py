84a8fe2dd7a4eea9318b1af9638af25cefc120e8
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-08-31 23:09:32 +0800

    Update DAO file

--- journal/dao/entries.py
+++ journal/dao/entries.py
@@ -4,9 +4,24 @@ class EntryDAO:
     def __init__(self, driver):
         self.driver = driver
     
+<<<<<<< Updated upstream
     def list_entries(self, user_id):
         with self.driver.session() as session:
             return session.read_transaction(self._list_entries, user_id)
+=======
+    def list_entries(self):
+        with self.driver.session() as session:
+            return session.read_transaction(self._list_entries_tx)
+    
+    def _list_entries_tx(self, tx):
+        query = """
+        MATCH (j:JournalEntry)
+        RETURN j
+        """
+        result = tx.run(query)
+        return [record["j"] for record in result]
+    
+>>>>>>> Stashed changes
 
     @staticmethod
     def _list_entries(tx, user_id):
